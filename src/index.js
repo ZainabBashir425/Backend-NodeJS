860b6288fc9148b859c27a38dc9f48ac4dafd4d8
ZainabBashir425 <zainabbashir785@gmail.com>
2025-07-21 18:37:04 +0500

    User Registration Added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-
-
+import {app} from "./app.js";
 dotenv.config({
-    path:'./env' // Ensure the path to your .env file is correct
+    path:'./.env' // Ensure the path to your .env file is correct
 });
 
 connectDB()
 .then(() => {
-    app.listem(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is running on port ${process.env.PORT || 8000}`);
     })
 })
